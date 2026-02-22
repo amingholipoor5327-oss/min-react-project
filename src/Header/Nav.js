986ec5dfa7f30df1dccaeda597389087c8da0259
amin <amingholipoor5327@gamil.com>
2026-02-22 23:12:04 +0330

    Edit navigation

--- src/Header/Nav.js
+++ src/Header/Nav.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom"
+import "./Nav.css"
 export const Nav = ()=>{
-    return(<div>
-        
-    </div>)
+    return(
+        <div className="nav">
+            <div className="nav-container">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/about" className="nav-link">About</Link>
+                <Link to="/contact" className="nav-link">Contact</Link>
+            </div>
+        </div>
+    )
 }
