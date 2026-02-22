4f389e770b23bfda2470c2b3a7e3fb5824bc5734
amin <amingholipoor5327@gamil.com>
2026-02-22 23:25:25 +0330

    Edit home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,5 +1,32 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import "./home.css"
 export const Home = ()=>{
-    return(<div>
-        
-    </div>)
+
+    const [data , setData ] = useState("")
+
+    const getData = ()=>{
+        axios.get("https://catfact.ninja/fact")
+        .then((res)=>{
+            setData(res.data.fact)
+        })
+    }
+
+    useEffect(()=>{
+        getData()
+    }, [])
+
+return (
+  <div className="home-container">
+    <h1>🐱 Random Cat Fact</h1>
+
+    <div className="fact-box">
+      {data ? data : "Click the button to get a fact"}
+    </div>
+
+    <button onClick={getData}>
+      Get Cat Fact
+    </button>
+  </div>
+);
 }
