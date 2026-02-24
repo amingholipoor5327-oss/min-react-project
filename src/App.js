9a199e1ae004f65a39a8f20fddb6a2cce307e3da
amin <amingholipoor5327@gamil.com>
2026-02-24 23:01:59 +0330

    queryclient

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import "./App.css";
 import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
 import { Home } from "./pages/Home/home";
 import { About } from "./pages/About/about";
+import { Nav } from "./Header/Nav";
 import { Contact } from "./pages/Contact/contact";
+import { QueryClientProvider , QueryClient } from "@tanstack/react-query";
  function App() {
+  const Client = new QueryClient()
    return (
     <div className="App">
+      <QueryClientProvider client={Client}>
        <Router>
         <Nav/> 
-        <Welcome/>
-        <Routes>
+         <Routes>
           <Route></Route>
           <Route path="/" element={<Home/>}> </Route>
           <Route path="/contact" element={<Contact/>}> </Route>
           <Route path="/about" element={<About/>}> </Route>
         </Routes>
       </Router>
+      </QueryClientProvider>
      </div>
   );
 }
