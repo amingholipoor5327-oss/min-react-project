aeb2544f2c058c13cca63c09a6d27143fe5d9b62
amin <amingholipoor5327@gamil.com>
2026-02-26 10:21:12 +0330

    added contact us pages

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -1,5 +1,21 @@
-export const Contact = ()=>{
-    return(<div>
-        
-    </div>)
+import "./contact.css"
+
+export const Contact = () => {
+  return (
+    <div className="contact-container">
+
+      <h1>Contact Me</h1>
+      <p className="contact-subtitle">
+        Feel free to reach out for collaboration or questions.
+      </p>
+
+      <form className="contact-form">
+        <input type="text" placeholder="Your Name" required />
+        <input type="email" placeholder="Your Email" required />
+        <textarea placeholder="Your Message" rows="5" required></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+        <span>call number : 0917-803-6862</span>
+    </div>
+  )
 }
