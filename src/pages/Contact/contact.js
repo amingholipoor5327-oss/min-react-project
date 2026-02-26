a44237b2705f0018faeb48961bda9704b206589f
amin <amingholipoor5327@gamil.com>
2026-02-26 11:03:07 +0330

    added react-hook-form

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -1,6 +1,31 @@
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { useForm } from "react-hook-form"
 import "./contact.css"
 
 export const Contact = () => {
+
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .min(4, "Name must be at least 4 characters" )
+      .max(20, "Name is too long")
+      .required("Name is required"),
+
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("Email is required"),
+  })
+
+  const {register , handleSubmit , formState: { errors }} = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const handleForm = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="contact-container">
 
@@ -9,13 +34,33 @@ export const Contact = () => {
         Feel free to reach out for collaboration or questions.
       </p>
 
-      <form className="contact-form">
-        <input type="text" placeholder="Your Name" required />
-        <input type="email" placeholder="Your Email" required />
-        <textarea placeholder="Your Message" rows="5" required></textarea>
+      <form className="contact-form" onSubmit={handleSubmit(handleForm)}>
+
+        <input
+          type="text"
+          placeholder="Name..."
+          {...register("name")}
+        />
+        {errors.name && <p className="error">{errors.name.message}</p>}
+
+        <input
+          type="email"
+          placeholder="Email..."
+          {...register("email")}
+        />
+        {errors.email && <p className="error">{errors.email.message}</p>}
+
+        <textarea
+          placeholder="Your Message"
+          rows="5"
+        ></textarea>
+
         <button type="submit">Send Message</button>
+
       </form>
-        <span>call number : 0917-803-6862</span>
+
+      <span>call number : 0917-803-6862</span>
+
     </div>
   )
 }
